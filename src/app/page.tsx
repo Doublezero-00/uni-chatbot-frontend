5fa2094b69708675bc9cef2b2e79d77b504eca26
Doublezero-00 <2022t01550@stu.cmb.ac.lk>
2026-06-18 23:01:49 +0530

    Changed api calling url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch(`${process.env.BACKEND_URL}/chat`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
